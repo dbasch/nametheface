60ae9304a4b15502842e426f0c79579b06706f2f
Diego Basch <dbasch@gmail.com>
2011-11-24 21:18:14 -0800

    first working version

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require 'sinatra'
 require 'haml'
 require 'linkedin'
 require 'yaml'
+require 'sqlite3'
 
 CONFIG = YAML.load_file 'config.yml'
 api_key = CONFIG['api_key']
@@ -12,20 +13,28 @@ enable :sessions
 
 get '/' do
   client = LinkedIn::Client.new(api_key, secret_key)
-  rtoken = client.request_token(:oauth_callback => "http://nametheface.heroku.com/name")
-  rsecret = rtoken.secret
-  session[:rtoken] = rtoken
-  session[:rsecret] = rsecret
-  redirect rtoken.authorize_url
-  
+  if session[:credentials] == nil
+    rtoken = client.request_token(:oauth_callback => "http://nametheface.heroku.com/authorize")
+    rsecret = rtoken.secret
+    session[:rtoken] = rtoken
+    session[:rsecret] = rsecret
+    redirect rtoken.authorize_url
+  else
+    client.authorize_from_access(session[:credentials][0], session[:credentials][1])
+    conns = client.connections
+    pic = nil
+    while pic == nil
+      @c= conns[:all][rand(conns[:total])]
+      pic = @c[:picture_url]
+    end
+    haml :index
+  end  
 end 
 
-get '/name' do
-  pin = params['oauth_verifier']
+get '/authorize' do
+  pin = params[:oauth_verifier]
   client = LinkedIn::Client.new(api_key, secret_key)
-  credentials = client.authorize_from_request(params['oauth_token'], session[:rsecret], pin)
+  credentials = client.authorize_from_request(params[:oauth_token], session[:rsecret], pin)
   session[:credentials] = credentials
-  #client.connections['all'][10]
-  conns = client.connections
-  conns['all'][rand(cons['total'])]
 end
+
