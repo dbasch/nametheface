ea31fd88cfd4699d730e3f6f0027dd61728453f9
Diego Basch <dbasch@gmail.com>
2011-11-25 08:55:42 -0800

    changed style a bit

--- app.rb
+++ app.rb
@@ -7,27 +7,27 @@ require 'yaml'
 CONFIG = YAML.load_file 'config.yml'
 api_key = CONFIG['api_key']
 secret_key = CONFIG['secret_key']
+app_url = CONFIG['app_url']
 
 enable :sessions
 
 get '/' do
   client = LinkedIn::Client.new(api_key, secret_key)
   if session[:credentials] == nil
-    rtoken = client.request_token(:oauth_callback => "http://nametheface.heroku.com/authorize")
+    rtoken = client.request_token(:oauth_callback => app_url + "/authorize")
     rsecret = rtoken.secret
     session[:rtoken] = rtoken
     session[:rsecret] = rsecret
     redirect rtoken.authorize_url
-  else
-    client.authorize_from_access(session[:credentials][0], session[:credentials][1])
-    conns = client.connections
-    pic = nil
-    while pic == nil
-      @c= conns[:all][rand(conns[:total])]
-      pic = @c[:picture_url]
-    end
-    haml :index
-  end  
+  end
+  client.authorize_from_access(session[:credentials][0], session[:credentials][1])
+  conns = client.connections
+  pic = nil
+  while pic == nil
+    @c= conns[:all][rand(conns[:total])]
+    pic = @c[:picture_url]
+  end
+  haml :index  
 end 
 
 get '/authorize' do
